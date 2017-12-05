b70fc84e7801680da451facdc81b735dee481e84
rmbarley <rmbarley@gmail.com>
2017-12-05 10:01:25 -0500

    add hotspots

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -246,24 +246,24 @@ $(window).on('hidden.bs.modal', function (e) {
 })
 
 // lock phones in landscape mode
-var start = function() {
-  screen.orientation.lock('landscape-primary').then(
-    startInternal,
-    function() {
-      alert('To start, rotate your screen to landscape.');
-
-      var orientationChangeHandler = function() {
-        if (!screen.orientation.type.startsWith('landscape')) {
-          return;
-        }
-        screen.orientation.removeEventListener('change', orientationChangeHandler);
-        startInternal();
-      }
-
-      screen.orientation.addEventListener('change', orientationChangeHandler);
-    });
-}
-window.onload = start;
+// var start = function() {
+//   screen.orientation.lock('landscape-primary').then(
+//     startInternal,
+//     function() {
+//       alert('To start, rotate your screen to landscape.');
+//
+//       var orientationChangeHandler = function() {
+//         if (!screen.orientation.type.startsWith('landscape')) {
+//           return;
+//         }
+//         screen.orientation.removeEventListener('change', orientationChangeHandler);
+//         startInternal();
+//       }
+//
+//       screen.orientation.addEventListener('change', orientationChangeHandler);
+//     });
+// }
+// window.onload = start;
 
 
 /***/ })
